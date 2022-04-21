d2dfceeec1bef7cf8ba09b721891c1a2815421fd
Kacper Pawlak <Kacper.Pawlak@365.aplitt.pl>
2022-04-21 18:08:17 +0200

    wszystko działa oprócz jednego seek bara

--- wpf_21_04/MainWindow.xaml.cs
+++ wpf_21_04/MainWindow.xaml.cs
@@ -26,72 +26,84 @@ namespace wpf_20_04_22
         {
             thecactusrec.Visibility = Visibility.Visible;
             thecactussli.Visibility = Visibility.Visible;
+            thecactussb.Visibility = Visibility.Visible;
             cactusprogress.Value++;
         }
         private void thecactuscheck_unchecked(object sender, RoutedEventArgs e)
         {
             thecactusrec.Visibility = Visibility.Hidden;
             thecactussli.Visibility = Visibility.Hidden;
+            thecactussb.Visibility = Visibility.Hidden;
             cactusprogress.Value--;
         }
         private void bestcactuscheck_checked(object sender, RoutedEventArgs e)
         {
             bestcactusrec.Visibility = Visibility.Visible;
             bestcactusli.Visibility = Visibility.Visible;
+            bestcactusb.Visibility = Visibility.Visible;
             cactusprogress.Value++;
         }
         private void bestcactuscheck_unchecked(object sender, RoutedEventArgs e)
         {
             bestcactusrec.Visibility = Visibility.Hidden;
             bestcactusli.Visibility = Visibility.Hidden;
+            bestcactusb.Visibility = Visibility.Hidden;
             cactusprogress.Value--;
         }
         private void cactusescheck_checked(object sender, RoutedEventArgs e)
         {
             cactusesrec.Visibility = Visibility.Visible;
             cactusesli.Visibility = Visibility.Visible;
+            cactusesb.Visibility = Visibility.Visible;
             cactusprogress.Value++;
         }
         private void cactusescheck_unchecked(object sender, RoutedEventArgs e)
         {
             cactusesrec.Visibility = Visibility.Hidden;
             cactusesli.Visibility = Visibility.Hidden;
+            cactusesb.Visibility = Visibility.Hidden;
             cactusprogress.Value--;
         }
         private void icecreamcheck_checked(object sender, RoutedEventArgs e)
         {
             icecreamrec.Visibility = Visibility.Visible;
             icecreamli.Visibility = Visibility.Visible;
+            icecreamsb.Visibility = Visibility.Visible;
             cactusprogress.Value++;
         }
         private void icecreamcheck_unchecked(object sender, RoutedEventArgs e)
         {
             icecreamrec.Visibility = Visibility.Hidden;
             icecreamli.Visibility = Visibility.Hidden;
+            icecreamsb.Visibility = Visibility.Hidden;
             cactusprogress.Value--;
         }
         private void stockcactuscheck_checked(object sender, RoutedEventArgs e)
         {
             stockcactusrec.Visibility = Visibility.Visible;
             stockcactusli.Visibility = Visibility.Visible;
+            stockcactusb.Visibility = Visibility.Visible;
             cactusprogress.Value++;
         }
         private void stockcactuscheck_unchecked(object sender, RoutedEventArgs e)
         {
             stockcactusrec.Visibility = Visibility.Hidden;
             stockcactusli.Visibility = Visibility.Hidden;
+            stockcactusb.Visibility = Visibility.Hidden;
             cactusprogress.Value--;
         }
         private void paintingcactuscheck_checked(object sender, RoutedEventArgs e)
         {
             paintingcactusrec.Visibility = Visibility.Visible;
             paintingcactusli.Visibility = Visibility.Visible;
+            paintingcactusb.Visibility = Visibility.Visible;
             cactusprogress.Value++;
         }
         private void paintingcactuscheck_unchecked(object sender, RoutedEventArgs e)
         {
             paintingcactusrec.Visibility = Visibility.Hidden;
             paintingcactusli.Visibility = Visibility.Hidden;
+            paintingcactusb.Visibility = Visibility.Hidden;
             cactusprogress.Value--;
         }
 
@@ -121,6 +133,31 @@ namespace wpf_20_04_22
         }
 
         private void thecactussb_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            thecactusrec.Width = thecactussb.Value;
+        }
+        private void tbestcactusli_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            bestcactusrec.Width = bestcactusb.Value;
+        }
+        private void cactusesb_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            cactusesrec.Width = cactusesb.Value;
+        }
+        private void icecreamsb_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            icecreamrec.Width = icecreamsb.Value;
+        }
+        private void stockcactusb_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            stockcactusrec.Width = stockcactusb.Value;
+        }
+        private void paintingcactusb_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            paintingcactusrec.Width = paintingcactusb.Value;
+        }
+
+        private void bestcactusb_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
 
         }
